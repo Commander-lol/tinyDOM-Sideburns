f027291b3bdfb38abb796744f65930bc35a867c3
Commander-lol <ljcapitanio@gmail.com>
2015-06-26 05:56:09 +0100

    Embedded Sideburns, reorganised file structure to reflect that

---
+++ src/tinyDOM-Sideburns.js
@@ -0,0 +1,37 @@
+if (!window.mu) {
+    throw new Error("tinyDOM-Sideburns requires tinyDOM to be in use on the page");
+}
+
+mu.templates = {};
+
+mu.render = function (path, data, options) {
+    if (mu.templates.hasOwnProperty(path)) {
+        return mu.templates[path](data, options);
+    } else {
+        return render(path, data, options);
+    }
+};
+
+
+mu.fn.render = function (path, data, options) {
+    var rendered = mu.render(path, data, options),
+        opts = options || {useHtml: true};
+    if (typeof opts.useHtml === "undefined") {
+        opts.useHtml = true;
+    }
+
+    this.each(function (i, e) {
+        if (opts.useHtml) {
+            e.innerHTML = rendered;
+        } else {
+            e.textContent = rendered;
+        }
+    });
+};
+
+mu.ready(function () {
+    mu("[type='x-template/sideburns']").each(function (i, e) {
+        var name = e.getAttribute("data-name");
+        mu.templates[name] = render.partial(e.textContent);
+    });
+});
